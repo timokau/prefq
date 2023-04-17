c5def945c2092ae2264b7b1a5306f0cf7c44ff18
Timo Kaufmann <timokau@zoho.com>
2023-04-17 15:04:27 +0200

    Add example script for data generation

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # prefq -- Querying Preferences from Real Humans
 
+## Running example scripts
+
+```bash
+poetry run python -m prefq.examples.random_recordings
+```
+
 ## Contributing
 
 This project uses [pre-commit](https://pre-commit.com/) for initial quality and consistency checks. To run these checks on each commit, simply execute `poetry run pre-commit install`. These checks will also be run on each GitHub pull request.
---
+++ prefq/examples/random_recordings.py
@@ -0,0 +1,35 @@
+"Generate recordings of random actions in gym environments." ""
+
+import gymnasium as gym
+
+
+def generate_random_episode_recordings(env_name, video_folder, num_episodes):
+    """Generate recordings of random actions in gym environments."""
+    env = gym.make(env_name, render_mode="rgb_array")
+    env = gym.wrappers.RecordVideo(
+        env, video_folder=video_folder, episode_trigger=lambda _: True
+    )
+
+    for _ in range(num_episodes):
+        # Run a random episode until termination
+        _observation, _info = env.reset()
+        while True:
+            # print("Step")
+            # Sample an action
+            action = env.action_space.sample()
+            # Execute it, observe the results
+            _observation, _reward, terminated, truncated, _info = env.step(action)
+
+            if terminated or truncated:
+                break
+    print("Closing")
+    env.close()
+
+
+def main():
+    """Run the script with default arguments"""
+    generate_random_episode_recordings("LunarLander-v2", "./videos", 10)
+
+
+if __name__ == "__main__":
+    main()
