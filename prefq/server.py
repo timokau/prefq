4a993b2a9e9d87a0294b06f4d31878d54bc9ff7e
ru96xed <ru96xed@campus.lmu.de>
2023-05-16 09:19:15 -0600

    Send preference feedback from web interface to server

--- prefq/server.py
+++ prefq/server.py
@@ -1,15 +1,27 @@
 """A webserver that asks the user preference queries."""
 
 import flask
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
-# defines the path of index
-@app.route("/")
+@app.route("/", methods=["POST"])
+def receive_data():
+    """Receives labels from the client (web_interface.js)"""
+    # Request.json object represents incoming client http request in json format
+    data = flask.request.json
+    # Extract JSON data sent by the client (web_interface.js)
+    is_left_preferred = data["is_left_preferred"]
+
+    print("Left Preferred: ", is_left_preferred)
+
+    return jsonify({"success": True})
+
+
+@app.route("/", methods=["GET"])
 def index():
-    """Serve the webinterface."""
+    """Defines index path (url) and renders html template"""
     return flask.render_template("web_interface.html")
 
 
