d8e486a93074880adae71f4ca5a978a3ab0873ce
patrickab <patrick.a.bitzer@gmail.com>
2023-11-02 18:22:21 +0100

    Remove Obsolete Condition

--- scripts/run_imitation_example.sh
+++ scripts/run_imitation_example.sh
@@ -3,11 +3,6 @@
 set -e
 set -o pipefail
 
-# First check if we can already install the full environment without issues. In
-# that case we are done. This is the case if the environment was already
-# installed previously, i.e., this speeds up the process on the second run.
-if ! poetry install -E examples; then
-	poetry install -E examples
-fi
+poetry install -E examples
 
 poetry run python3 -m prefq.examples.imitation_preference_comparisons
